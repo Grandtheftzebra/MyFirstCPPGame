39318f2dde5be55649668f6c263442dee2c00681
marcr <marc.rydzy@gmail.com>
2026-03-26 18:50:49 +0100

    created assetManager

--- src/game/gameMain.cpp
+++ src/game/gameMain.cpp
@@ -7,31 +7,27 @@
 
 struct GameData
 {
-    float PosX = 100;
-    float PosY = 100;
-    float MoveSpeed = 200;
-}gameData;
+    Texture DirtTexture;
+} gameData;
 
 bool InitGame()
 {
+    gameData.DirtTexture = LoadTexture(RESOURCES_PATH "dirt.png");
+
     return true;
 }
 
 constexpr float MaxDeltaTime = 1.f / 5.f;
+
 bool UpdateGame()
 {
     float deltaTime = GetFrameTime();
-    if (deltaTime > MaxDeltaTime) {deltaTime = MaxDeltaTime;}
+    if (deltaTime > MaxDeltaTime) { deltaTime = MaxDeltaTime; }
 
-    permaAssertCommentDevelopement(deltaTime > 10.f, "test");
-
-    if (IsKeyDown(KEY_D)) {gameData.PosX += gameData.MoveSpeed * deltaTime;}
-    if (IsKeyDown(KEY_A)) {gameData.PosX -= gameData.MoveSpeed * deltaTime;}
-    // Movement inverted for W and S
-    if (IsKeyDown(KEY_W)) {gameData.PosY -= gameData.MoveSpeed * deltaTime;}
-    if (IsKeyDown(KEY_S)) {gameData.PosY += gameData.MoveSpeed * deltaTime;}
-
-    DrawRectangle(gameData.PosX, gameData.PosY, 50, 50, RED);
+    DrawTexturePro(gameData.DirtTexture, {
+                       0, 0, (float) gameData.DirtTexture.width,
+                       (float) gameData.DirtTexture.height
+                   }, {500, 500, 100, 100}, {0, 0}, 0, WHITE);
 
     return true;
 }
